04d07e4bef56bbe925881d77d646c7b71c18b34e
chrisg06 <64582286+chrisg06@users.noreply.github.com>
2023-05-07 18:54:04 +0800

    Create navbar.vue

---
+++ src/components/navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="">
+    <nav class="py-10 mb-12 flex justify-between">
+      <h1
+        class="text-xl bg-gradient-to-r from-blue-500 via-green-400 to-indigo-400 inline-block text-transparent bg-clip-text"
+      >
+        VATSIM Pilot Assist
+      </h1>
+    </nav>
+  </section>
+</template>
