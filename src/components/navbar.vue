99fbe461c820eb43e9880e54e9a0083c49c1d853
chrisg06 <64582286+chrisg06@users.noreply.github.com>
2023-05-12 18:56:47 +0800

    add notams to navbar

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -8,7 +8,7 @@
           to="/"
           class="text-xl bg-gradient-to-r from-blue-500 via-green-400 to-indigo-400 inline-block text-transparent bg-clip-text"
         >
-          VATSIM Pilot Assist
+          VATPAC Pilot Assist
         </router-link>
         <div class="flex md:order-2">
           <button
@@ -57,10 +57,11 @@
               >
             </li>
             <li>
-              <a
+              <router-link
+                to="/notams"
                 href="#"
                 class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >NOTAMs</a
+                >NOTAMs</router-link
               >
             </li>
             <li>
--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import home from './views/home.vue'
 import weather from './views/weather.vue'
+import notam from './views/notam.vue'
 
-import routeCard from './components/routeCard.vue'
 import { createRouter, createWebHistory, type RouteRecordRaw} from 'vue-router'
 
 
 const routes: Array<RouteRecordRaw> = [
     {path: '/', component: home},
     { path: '/weather', component: weather },
+    { path: '/notams', component: notam}
 ]
 
 const router = createRouter({
