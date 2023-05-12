7ae4bbd8a825d31e93c2df56a2c7d8bd91d458d8
chrisg06 <64582286+chrisg06@users.noreply.github.com>
2023-05-12 18:56:27 +0800

    restyle weather page

--- src/views/weather.vue
+++ src/views/weather.vue
@@ -57,16 +57,13 @@ export default {
 <template>
   <navbar></navbar>
   <main>
-    <div
-      class="min-w-full px-6 pb-6 pt-4 mt-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
-    >
+    <div class="px-6 pb-6 pt-4 mt-6 bg-white">
       <h1>
-        <h5 class="mb-4 text-4xl font-bold flex justify-center text-gray-900 dark:text-white">
-          Weather Finder
-        </h5>
+        <h5 class="mb-2 text-4xl font-bold">Weather Finder</h5>
+        <div class="h-1 w-64 bg-blue-300 rounded"></div>
       </h1>
 
-      <form class="flex flex-col" @submit.enter.prevent >
+      <form class="flex flex-col mt-12" @submit.enter.prevent>
         <div class="flex items-center">
           <div class="relative w-full">
             <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -84,7 +81,8 @@ export default {
                 ></path>
               </svg>
             </div>
-            <input @keyup.enter="fetchData"
+            <input
+              @keyup.enter="fetchData"
               type="text"
               v-model="icao"
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -122,72 +120,80 @@ export default {
               </div>
             </div>
           </li>
-          <li>
-            <div class="grid grid-cols-2 md:flex-row gap-8 pb-5">
-              <div class="flex-grow border border-blue-300 overflow-hidden shadow-md rounded-lg">
-                <div class="px-4 py-5 sm:p-6 mx-auto">
-                  <h3 class="text-sm font-medium text-gray-500 truncate">Wind Direction</h3>
-                  <p class="mt-1 text-xl font-semibold text-gray-900">{{ wind.direction }}</p>
-                </div>
+
+          
+          <div class="grid grid-cols-2 gap-4">          
+
+            <li
+            v-if="wind.direction"
+             class="flex-grow border border-blue-300 overflow-hidden shadow-md rounded-lg">
+              <div class="px-4 py-5 sm:p-6 mx-auto">
+                <h3 class="text-sm font-medium text-gray-500 truncate">Wind Direction</h3>
+                <p class="mt-1 text-xl font-semibold text-gray-900">{{ wind.direction }}</p>
               </div>
-              <div class="flex-grow border border-blue-300 overflow-hidden shadow-md rounded-lg">
+            </li>
+              <li
+              v-if="wind.speed" class="flex-grow border border-blue-300 overflow-hidden shadow-md rounded-lg">
                 <div class="px-4 py-5 sm:p-6 mx-auto">
                   <h3 class="text-sm font-medium text-gray-500 truncate">Wind Speed</h3>
                   <p class="mt-1 text-xl font-semibold text-gray-900">{{ wind.speed }}</p>
                 </div>
+              </li>
+            <li 
+            v-if="temp" class="flex-grow border border-blue-300 overflow-hidden shadow-md rounded-lg">
+              <div class="px-4 py-5 sm:p-6 mx-auto">
+                <h3 class="text-sm font-medium text-gray-500 truncate">Temperature</h3>
+                <p class="mt-1 text-xl font-semibold text-gray-900">{{ temp }}</p>
               </div>
-            </div>
-            <div class="grid grid-cols-2 md:flex-row gap-8">
-              <div class="flex-grow border border-blue-300 overflow-hidden shadow-md rounded-lg">
-                <div class="px-4 py-5 sm:p-6 mx-auto">
-                  <h3 class="text-sm font-medium text-gray-500 truncate">Temperature</h3>
-                  <p class="mt-1 text-xl font-semibold text-gray-900">{{ temp }}</p>
-                </div>
+            </li>
+            <li
+            v-if="dewpoint" class="flex-grow border border-blue-300 overflow-hidden shadow-md rounded-lg">
+              <div class="px-4 py-5 sm:p-6 mx-auto">
+                <h3 class="text-sm font-medium text-gray-500 truncate">Dewpoint</h3>
+                <p class="mt-1 text-xl font-semibold text-gray-900">{{ dewpoint }}</p>
               </div>
-              <div class="flex-grow border border-blue-300 overflow-hidden shadow-md rounded-lg">
-                <div class="px-4 py-5 sm:p-6 mx-auto">
-                  <h3 class="text-sm font-medium text-gray-500 truncate">Dewpoint</h3>
-                  <p class="mt-1 text-xl font-semibold text-gray-900">{{ dewpoint }}</p>
-                </div>
-              </div>
-            </div>
-          </li>
+            </li>
 
-          <li>
-            <div class="grid grid-cols-2 md:flex-row gap-8">
-              <div class="flex-grow border border-blue-300 overflow-hidden shadow-md rounded-lg">
-                <div class="px-4 py-5 sm:p-6 mx-auto">
-                  <h3 class="text-sm font-medium text-gray-500 truncate">Weather</h3>
-                  <p class="mt-1 text-xl font-semibold text-gray-900" v-for="weather in wx.wx">{{ weather }}</p>
+            <li
+              v-if="wx.wx !== null"
+              class="flex-grow border border-blue-300 overflow-hidden shadow-md rounded-lg"
+            >
+              <div class="px-4 py-5 sm:p-6 mx-auto">
+                <h3 class="text-sm font-medium text-gray-500 truncate">Weather</h3>
+                <p class="mt-1 text-xl font-semibold text-gray-900" v-for="weather in wx.wx">
+                  {{ weather }}
+                </p>
+              </div>
+            </li>
+            <li
+                v-if="wx.cld"
+             class="flex-grow border border-blue-300 overflow-hidden shadow-md rounded-lg">
+              <div class="px-4 py-5 sm:p-6 mx-auto">
+                <h3 class="text-sm font-medium text-gray-500 truncate">Cloud</h3>
+                <div class="flex justify-start">
+                  <p class="mt-1 text-xl font-semibold text-gray-900 mr-3" v-for="cld in wx.cld">
+                    {{ cld }}
+                  </p>
                 </div>
               </div>
-              <div class="flex-grow border border-blue-300 overflow-hidden shadow-md rounded-lg">
-                <div class="px-4 py-5 sm:p-6 mx-auto">
-                  <h3 class="text-sm font-medium text-gray-500 truncate">Cloud</h3>
-                  <div class="flex justify-start">
-                    <p class="mt-1 text-xl font-semibold text-gray-900 mr-3" v-for="cld in wx.cld">{{ cld }}</p>
-                  </div>
-
-                </div>
+            </li>
+            <li
+                v-if="wx.vis"
+             class="flex-grow border border-blue-300 overflow-hidden shadow-md rounded-lg">
+              <div class="px-4 py-5 sm:p-6 mx-auto">
+                <h3 class="text-sm font-medium text-gray-500 truncate">Visibility</h3>
+                <p class="mt-1 text-xl font-semibold text-gray-900">{{ wx.vis }}</p>
               </div>
-            </div>
-          </li>
-          <li>
-            <div class="grid grid-cols-2 md:flex-row gap-8">
-              <div class="flex-grow border border-blue-300 overflow-hidden shadow-md rounded-lg">
-                <div class="px-4 py-5 sm:p-6 mx-auto">
-                  <h3 class="text-sm font-medium text-gray-500 truncate">Visibility</h3>
-                  <p class="mt-1 text-xl font-semibold text-gray-900">{{ wx.vis }}</p>
-                </div>
+            </li>
+            <li
+                v-if="qnh"
+             class="flex-grow border border-blue-300 overflow-hidden shadow-md rounded-lg">
+              <div class="px-4 py-5 sm:p-6 mx-auto">
+                <h3 class="text-sm font-medium text-gray-500 truncate">QNH</h3>
+                <p class="mt-1 text-xl font-semibold text-gray-900">{{ qnh }}</p>
               </div>
-              <div class="flex-grow border border-blue-300 overflow-hidden shadow-md rounded-lg">
-                <div class="px-4 py-5 sm:p-6 mx-auto">
-                  <h3 class="text-sm font-medium text-gray-500 truncate">QNH</h3>
-                  <p class="mt-1 text-xl font-semibold text-gray-900">{{ qnh }}</p>
-                </div>
-              </div>
-            </div>
-          </li>
+            </li>
+        </div>
 
         </ul>
       </form>
