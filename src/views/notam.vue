ece03a5a413cabc8de0af8dcd903285ea0c54680
chrisg06 <64582286+chrisg06@users.noreply.github.com>
2023-05-12 18:56:39 +0800

    create notam page

---
+++ src/views/notam.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import navbar from '../components/navbar.vue'
+</script>
+
+<template>
+    <navbar></navbar>
+    <main>
+      <div class="px-6 pb-6 pt-4 mt-6 bg-white">
+        <h1>
+          <h5 class="mb-2 text-4xl font-bold">NOTAMS</h5>
+          <div class="h-1 w-64 bg-blue-300 rounded"></div>
+          <p class="mt-4 text-3xl">Coming Soon</p>
+        </h1>
+      </div>
+    </main>
+</template>
